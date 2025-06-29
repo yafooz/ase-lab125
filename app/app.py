68175764ca413a7c475fea84f53d658a2b8e1929
yafooz <yousefarar1981@gmail.com>
2025-06-30 00:23:53 +0200

    add functions lower ,upper,reduce,concat

--- app/app.py
+++ app/app.py
@@ -85,7 +85,68 @@ def factorial():
      else:
        return make_response(jsonify("bad request"),400)
      
-     
+
+
+@app.route("/upper")
+def upper():
+    text=request.args.get('text',type=str)
+    if text:
+        return make_response(jsonify(text=text.upper()),200)
+    else:
+        return make_response(jsonify("bad request"),400)
+
+@app.route("/lower")
+def lower():
+    text=request.args.get('text',type=str)
+    if text:
+        return make_response(jsonify(text=text.lower()),200)
+    else:
+        return make_response(jsonify("bad request"),400)
+    
+@app.route("/concat")
+def concat():
+    text1=request.args.get('text1',type=str)
+    text2=request.args.get('text2',type=str)
+    if text1 and text2:
+        return make_response(jsonify(text=text1+text2),200)
+    else:
+        return make_response(jsonify("bad request"),400)
+
+@app.route("/reduce")
+def reduce(op,lst):
+    if op == 'add':
+        result = sum(lst)
+    elif op == 'mul':
+        result = 1
+        for num in lst:
+            result *= num
+
+    elif op == 'div':
+        for num in lst:
+            if num == 0:
+                return make_response(jsonify("Division by zero error"), 400)
+            else:
+                result = lst[0]
+                for num in lst[1:]:
+                    result /= num
+            result /= num
+
+    elif op == 'mod':
+        for num in lst:
+            result=num
+            result  = result % num
+
+    else:
+        return make_response(jsonify("Invalid operation"), 400)
+    
+    return make_response(jsonify(s=result), 200)
+
+
+@app.route("/crash")
+def crash():
+    try
+        
+
 if __name__ == '__main__':
     app.run(debug=True)
     
